eafce01eb3768aa4e2a9e534d6023c91d33479c3
gurjo <gurjo@10.48.17.247>
2023-05-25 13:12:35 -0400

    DEMO

---
+++ src/javaapplication2/pkg95/JavaApplication295.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2.pkg95;
+
+/**
+ *
+ * @author gurjo
+ */
+public class JavaApplication295 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
